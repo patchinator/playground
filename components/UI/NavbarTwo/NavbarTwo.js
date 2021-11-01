682dbdeac9e33e10cf45e1b50e2f703904d175b4
Patrick Hoveman <patchinator@hotmail.co.uk>
2021-11-01 17:08:29 +0000

    Added Icons to NavbarTwo component

--- components/UI/NavbarTwo/NavbarTwo.js
+++ components/UI/NavbarTwo/NavbarTwo.js
@@ -1,15 +1,27 @@
 import style from "./NavbarTwo.module.scss";
 import ButtonSlide2 from "../ButtonSlideTwo/ButtonSlideTwo";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faUser,
+  faCog,
+  faNewspaper,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
 
 const NavbarTwo = () => {
   return (
     <nav className={style.navbar}>
       <div className={style.navbar_button_wrapper}>
-        <ButtonSlide2>Profile</ButtonSlide2>
-        <ButtonSlide2>About</ButtonSlide2>
-        <ButtonSlide2>News</ButtonSlide2>
+        <ButtonSlide2>
+          <FontAwesomeIcon icon={faUser} width="1rem" />
+        </ButtonSlide2>
+        <ButtonSlide2>
+          <FontAwesomeIcon icon={faCog} width="1.25rem" />
+        </ButtonSlide2>
+        <ButtonSlide2>
+          <FontAwesomeIcon icon={faNewspaper} width="1.25rem" />
+        </ButtonSlide2>
       </div>
       <div className={style.navbar_search}>
         <input type="text" placeholder="Search our store..." />
